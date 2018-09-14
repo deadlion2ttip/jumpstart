12ae954ddd8ee2ea2f6c4f90b9ae03b9e48bdf08
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-09-14 10:51:45 -0400

    Now creates empty HTML and CSS files.

--- jumpstart/main.c
+++ jumpstart/main.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
     if (argv[1]){
-
+/* set up html file name */
         char fileName[30];
-        char fileEnd[] = ".html";
+        const char fileEnd[] = ".html";
         strcpy(fileName, argv[1]);
         strcat(fileName, fileEnd);
-        printf("%s", fileName);
 
+/* set up css file name*/
+        char cssName[30];
+        strcpy(cssName, argv[1]);
+        strcat(cssName, ".css");
 
+/*test if file already exists*/
        if(fopen(("%s", fileName), "r")){
         printf("File Already Exists. Canceling write.");
        }else {
+/*create new files*/
+        FILE * htmlPointer = fopen(("%s", fileName), "w");
 
-        FILE * fpointer = fopen(("%s", fileName), "w");
+
+        fclose(htmlPointer);
+
+        FILE * cssPointer = fopen(("%s", cssName), "w");
 
 
-        fclose(fpointer);
+        fclose(cssPointer);
        }
     } else {
         printf("Please provide a file name as the first argument.");
