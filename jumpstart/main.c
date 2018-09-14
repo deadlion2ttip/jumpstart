32be8fdef65230f14705352d7ffd4ed6f7402b49
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-09-13 22:03:13 -0400

    Can create a file with the chosen name.

--- jumpstart/main.c
+++ jumpstart/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+char *strcat(char *dest, const char *src);
+
+
+int main(int *argc, char *argv[])
 {
-     printf("%s %s", argv[1], argv[2]);
+    if (argv[1]){
+
+        char fileName[30];
+        char fileEnd[] = ".html";
+        strcpy(fileName, argv[1]);
+        strcat(fileName, fileEnd);
+        printf("%s", fileName);
+
+        FILE * fpointer = fopen(("%s", fileName), "w");
+
+
+        fclose(fpointer);
+    } else {
+        printf("Please provide a file name as the first argument.");
+    }
     return 0;
 }
